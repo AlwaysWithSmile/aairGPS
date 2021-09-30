775dc0505c5a141d434acee28847b4588505ebb6
SemKaminskyi <donbod8@gmail.com>
2021-09-30 12:01:53 +0300

    Update grreact.go

--- grreact.go
+++ grreact.go
@@ -273,14 +273,15 @@ func decodeGpsJson(jsonIncoming string, conn *websocket.Conn) string {
 	"obname":"Трансформаторная подстанция ",
 	"obtel":"",
 	"pult":"123",
-	"status":""
+	"status":"",
+	"waymark":"\u043e\u0431\u044a\u0435\u043a\u0442 \u043d\u0430\u0445\u043e\u0434\u0438\u0442\u0441\u044f \u043f\u043e \u0430\u0434\u0440\u0435\u0441\u0443 \u0443\u043b\u0438\u0446\u0430 \u041a\u0440\u0430\u0441\u043d\u043e\u0442\u043a\u0430\u0446\u043a\u0430\u044f, 40 \u043d\u0430 \u0442\u0435\u0440\u0440\u0438\u0442\u043e\u0440\u0438\u0438 \u041f\u041f \u041c\u0438\u043a\u0441\u0435\u0440. \u041f\u0435\u0440\u0435\u0434 \u0444\u0438\u0440\u043c\u0435\u043d\u043d\u044b\u043c \u043c\u0430\u0433\u0430\u0437\u0438\u043d\u043e\u043c \u041c\u0438\u043a\u0443\u043b\u0438\u043d\u0435\u0446\u043a\u0438\u0439 \u0431\u0440\u043e\u0432\u0430\u0440 \u043f\u043e\u0432\u0435\u0440\u043d\u0443\u0442\u044c \u043d\u0430 \u043f\u0440\u0430\u0432\u043e \u0438 \u043f\u0440\u043e\u0435\u0445\u0430\u0442\u044c 20\u043c\u0435\u0442\u0440\u043e\u0432. \u041e\u0434\u043d\u043e\u044d\u0442\u0430\u0436\u043d\u043e\u0435 \u0442\u0435\u0445\u043d\u0438\u0447\u0435\u0441\u043a\u043e\u0435 \u043a\u0430\u043f\u0438\u0442\u0430\u043b\u044c\u043d\u043e\u0435 \u0437\u0434\u0430\u043d\u0438\u0435"
 }
 `, &cardBase)
 			fmt.Println("Successfully connected....")
 			s := string(34)
 			fmt.Println(cardBase.ID)
 			convertedCardBaseID := strconv.Itoa(cardBase.ID)
-			s_json := "{" + s + "obinfo" + s + ":" + "[" + "{" + s + "id" + s + ":" + s + convertedCardBaseID + s + "," + s +"lat" + s  +":"+ s + cardBase.CARD_LAT + s +"," + s + "lon"+ s +":" + s + cardBase.CARD_LON + s +"," + s +"obadr"+ s +":" + s +cardBase.CARD_ADRES+ s +","+ s +"obname"+ s +":" + s +cardBase.CARD_NAME+ s +"," +  s + "pult" + s + ":"+ s + cardBase.CARD_PULTNUM+ s +"}" +"]"
+			s_json := "{" + s + "obinfo" + s + ":" + "[" + "{" + s + "id" + s + ":" + s + convertedCardBaseID + s + "," + s +"lat" + s  +":"+ s + cardBase.CARD_LAT + s +"," + s + "lon"+ s +":" + s + cardBase.CARD_LON + s +"," + s +"obadr"+ s +":" + s +cardBase.CARD_ADRES+ s +","+ s +"obname"+ s +":" + s +cardBase.CARD_NAME+ s +"," +  s + "pult" + s + ":"+ s + cardBase.CARD_PULTNUM+ s + "," + s + "details" + s + ":" + s + cardBase.CARD_WAYMARK + s +"}" +"]"
 			s_json += ","
 			s_json += s + "userlist" + s + ":" + "[" + "{" + s + "name" + s + ":"+ s + people.MAN_NAME + s + "," + "num" + ":" + s + people.MAN_NUM + s + "," + s + "tel" + s + ":" + s + people.MAN_PHONE + s + "}"+"]"
 			s_json += ","
